2e68b284cf8151b9b1c2164316f6ec202402174d
smitroy4 <smitroy22@gmail.com>
2025-09-19 09:54:34 +0530

    First Commit

---
+++ src/test/java/com/smit/SpringBoot_Web/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smit.SpringBoot_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
