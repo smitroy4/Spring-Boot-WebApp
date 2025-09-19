2f68d6da9b69780b10d034dfc9bda9d07d9b59e4
smitroy4 <smitroy22@gmail.com>
2025-09-19 09:59:14 +0530

    First Commit

--- src/main/java/com/smit/SpringBoot_Web/SpringBootWebApplication.java
+++ src/main/java/com/smit/SpringBoot_Web/SpringBootWebApplication.java
@@ -9,6 +9,7 @@ public class SpringBootWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
         System.out.println("Hello World!");
+        System.out.println("Hello World-test!");
 	}
 
 }
