5039469fea10fd66339c32e1b64f7a2243800846
smitroy4 <smitroy22@gmail.com>
2025-09-20 12:11:17 +0530

    Setup of Addition calculator using Servet

--- src/main/java/com/smit/SpringBoot_Web/SpringBootWebApplication.java
+++ src/main/java/com/smit/SpringBoot_Web/SpringBootWebApplication.java
@@ -8,8 +8,6 @@ public class SpringBootWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
-        System.out.println("Hello World!");
-        System.out.println("Hello World-test!");
 	}
 
 }
